7322272291ceed89a0966aa76e4d21ef7f6456ce
Umar Hasan <umrhsnE2020@gmail.com>
2022-08-24 20:21:08 +0200

    - used FragmentContainerView because fragment is deprecated. - also updated how to get navHostFragment in MainActivity.kt

--- app/src/main/java/com/umrhsn/shoestore/ui/activities/MainActivity.kt
+++ app/src/main/java/com/umrhsn/shoestore/ui/activities/MainActivity.kt
@@ -3,8 +3,11 @@ package com.umrhsn.shoestore.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -13,13 +16,15 @@ import com.umrhsn.shoestore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: Fragment
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        navController = this.findNavController(R.id.navHostFragment)
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment)!!
+        navController = navHostFragment.findNavController()
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
     }
